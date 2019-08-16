f90bb98286dcefcc45a40b56cb94907cc26734af
coltonhurst <colton@coltonhurst.com>
2019-08-16 11:33:16 -0400

    Add anonymous struct example with typedef

--- lang-examples/typedef.c
+++ lang-examples/typedef.c
@@ -14,6 +14,12 @@ struct a_struct_with_a_massively_large_name {
 
 typedef struct a_struct_with_a_massively_large_name THESTRUCT;
 
+/* We can also typdef "anonymous" structs */
+typedef struct {
+    float gas;
+    char * name;
+} car;
+
 int main(void)
 {
     /* make a variable of the structure like this: */
@@ -21,6 +27,13 @@ int main(void)
 
     /* OR, use the alias you made with typedef */
     THESTRUCT another_variable;
+
+    /* here we are using the anonymous struct 'car' that we gave an alias to */
+    car myCar;
+    myCar.gas = 32.2;
+    myCar.name = "The Red Roadster";
+
+    printf("The car called %s has %.2f gallons of gas left.\n", myCar.name, myCar.gas);
 
     return 0;
 }
